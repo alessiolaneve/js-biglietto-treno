31d5e742b32d248cb3d8bbd160f2c50c3d7b3356
alessiolaneve <alessioln98@gmail.com>
2024-03-11 17:40:57 +0100

    prezzo finito

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 console.log('biglietto del treno')
-
+const prezzoKm = 0.21
 // km percorsi
-
+const km = parseInt(prompt('Inserisci il numero di km del viaggio')) 
+console.log (km)
 // età del passeggero
-
+const age = parseInt(prompt('Inserisci la tua età')) 
+console.log (age)
 // prezzo totale, sconto < 18 o > 65
+const prezzo = prezzoKm * km
+console.log (prezzo)
 
+let sconto = 0
+
+if(age<18){
+    sconto = prezzo * 0.2
+} 
+else if (age=>65){
+    sconto = prezzo * 0.4
+} 
+
+let prezzoTot = prezzo - sconto
+console.log(prezzoTot)
 // prezzo a due decimali
